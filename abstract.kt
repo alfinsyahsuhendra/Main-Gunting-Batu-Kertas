bc661562d45ff24587d80b53c85480a57153af1e
Lenovo <rezaahatmi24@gmaiul.com>
2022-07-10 01:19:41 +0700

    abstract.kt

---
+++ abstract.kt
@@ -0,0 +1,16 @@
+abstract class Suit(val game: String ) {
+    init{
+        println("Mulai Bermain: $game")
+    }
+    fun resultGameWin(player: Int, description: String){
+        var playerWin = player+1
+        println("Hasil: ")
+        println("Pemain: $playerWin $description")
+    }
+    open fun getDrawResult(){
+        println("Hasil: ")
+        println("DRAW")
+    }
+    abstract fun restartGame()
+
+}
